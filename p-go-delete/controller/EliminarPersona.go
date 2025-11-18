01ced24f42ea79eda006544ff9157dcc02488d74
stiven122750 <franklim.cardona.7622@eam.edu.co>
2025-11-18 14:09:48 -0500

    update

---
+++ p-go-delete/controller/EliminarPersona.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/stiven122750/cruds-go/p-go-delete/services"
+	"github.com/gorilla/mux"
+)
+
+func EliminarPersona(w http.ResponseWriter, r *http.Request) {
+	// Obtener el documento desde los parámetros de la URL
+	vars := mux.Vars(r)
+	documento := vars["documento"]
+
+	if documento == "" {
+		http.Error(w, "El documento es obligatorio", http.StatusBadRequest)
+		return
+	}
+
+	// Llamar al servicio
+	err := services.EliminarPersona(documento)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"mensaje": "Persona eliminada exitosamente"})
+}
